0aa1a7d98bd7fdf37ad4d8d6d44159c97d221ec9
tangihan <78784964+tangihan@users.noreply.github.com>
2023-03-08 22:45:04 +0800

    integrate login api

---
+++ src/API/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+
+// const CARD_URL = "http://localhost:8081";
+
+
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,71 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Box, Link, TextField, Typography } from "@mui/material";
 import _ from "lodash";
+import jwt from "jwt-decode";
+import { HmacSHA256, enc} from "crypto-js";
+import { CognitoIdentityServiceProvider } from "aws-sdk";
 
 import "./Login.css";
 import CustomButton from "../../Components/CustomButton/CustomButton";
+
+const AWS_REGION = process.env.REACT_APP_REGION;
+const USER_POOL_ID = process.env.REACT_APP_USER_POOL_ID;
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+const cognito = new CognitoIdentityServiceProvider({
+    region: AWS_REGION,
+    userPoolId: USER_POOL_ID,
+    clientId: CLIENT_ID,
+});  
 
 const Login = () => {
 
     const navigate = useNavigate();
     const [formData, setFormData] = useState({});
-    const [error, setError] = useState({
-        email: false, password: false
-    })
+    const [error, setError] = useState();
 
-    const handleLogin = () => {
-        console.log(formData)
-        // const role = "customer";
-        const role = "tenant";
-
-        navigate(role === "tenant" ? "/datafiles" : "/pointsactivity")
+    const roleNavigation = (token) => {
+        const role = jwt(token)['cognito:groups'][0];
+        if (role === "tenant") {
+            navigate("/datafiles");
+        } else {
+            navigate("/rewards");
+        }
     }
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const params = {
+            AuthFlow: "USER_PASSWORD_AUTH",
+            ClientId: CLIENT_ID, 
+            AuthParameters: {
+              USERNAME: formData["email"],
+              PASSWORD: formData["password"],
+              SECRET_HASH: HmacSHA256(formData["email"] + CLIENT_ID, CLIENT_SECRET).toString(enc.Base64)
+            },
+        };  
+
+        cognito.initiateAuth(params, (err, data) => {
+            if (err) {
+                console.log("Login Failed: ", err.message);
+                setError(true);
+            } else {
+                const token = data.AuthenticationResult.IdToken;
+                localStorage.setItem("token", token);
+                roleNavigation(token);
+            }
+        })
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            roleNavigation(token);
+        }
+    });
 
     return (
         <Box className="background">
@@ -48,17 +91,15 @@ const Login = () => {
                             size="small"
                             fullWidth
                             placeholder={item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()}
-                            error={error[item]}
-                            helperText={
-                                error[item] && item ==="email" ? "Email does not exist" 
-                                : error[item] && item === "password" ? "Password Incorrect. Please Try Again"
-                                : ""
-                            }
+                            error={error}
+                            helperText={ error && item === "password" 
+                                        && "Incorrect email or password. Please Try again"}
                             onChange={(event) => {
                                 setFormData((state) => ({
                                     ...state, 
                                     [item]: event.target.value
                                 }))
+                                setError(false);
                             }}
                             sx={{
                                 my: 1.5,
@@ -89,13 +130,11 @@ const Login = () => {
                 <CustomButton 
                     text="Login"
                     fullWidth={true}
-                    handleOnClick={handleLogin}
+                    handleOnClick={onSubmit}
                     nameOfClass="customButton"
                 />    
             </Box>
-        </Box>
-
-       
+        </Box>       
     )
 }
 
